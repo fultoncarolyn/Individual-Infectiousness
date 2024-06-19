78d4f195fc14854d242724090c8fbbd64a02dce2
fultoncarolyn <157443948+fultoncarolyn@users.noreply.github.com>
2024-06-19 15:41:54 -0600

    Add files via upload

--- newsimjun19.R
+++ newsimjun19.R
@@ -1,23 +1,24 @@
 # Parameters
-beta = 0.175
-gamma = 0.5
+beta = 1.75
+gamma = 0.35
 N = 1000 
 
 # Populations
-I =   3  # number of infectious at time = 0
+I =   2  # number of infectious at time = 0
 
-input = integer(2)
+input = integer(I)
 
 funct <- function(vector){
   size = length(vector)
   #while (i <= size) {
     for (i in 1:N){
       tau = rexp(1,1/gamma) #infectiousness profile timing
-      print(paste0("New tau:", tau,"for iteration:", i,"."))
+      #print(paste0("New tau:", tau,"for iteration:", i,"."))
       upsilon = rpois(1,beta/gamma) #number of contacts within the infectiousness period
-      print(paste0("New upsilon:", upsilon,"for iteration:", i,"."))
+      #print(paste0("New upsilon:", upsilon,"for iteration:", i,"."))
+      ##mu = rbern(upsilon,S/N) #pribability of transmission (Need to talk through implementing this...)
       contacts = c()
-      contacts <- rep(tau,times = (upsilon))
+      contacts <- rep(vector[i]+tau,times = (upsilon))
       addsize = length(contacts)
       vector <- c(vector,contacts)
       newI = length(vector)
@@ -25,12 +26,22 @@ funct <- function(vector){
       newS = N - newI
       #print(paste0("New S pop:", newS,"for iteration:", i,"."))#Susceptible population tracking
       output <- sort(vector)
-      print(output)
+      #print(output)
       
       size <- size + addsize
       print(paste0("New size:", size,"for iteration:", i,"."))
+      if (size > N)
+        break
       if (i >= size)
         break
     }
-  print(output)
+  #print(output)
+  infecttime = output #what's our timescale?
+  infectppl = 1:length(output)
+  
+  plot(infecttime,infectppl)
+  
+  #with(infecteddata, {
+    #plot(infecteddata, y)
+    #}
 }
